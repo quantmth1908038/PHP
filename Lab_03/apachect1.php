84238a1b25d6eadaf68545b2f3c7b70a9f9e8c79
quantmth1908038 <quantmth1908038@fpt.edu.vn>
2020-06-09 00:50:29 +0700

    Lab_03

---
+++ Lab_03/apachect1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>String Assignment Operation</title>
+</head>
+<body>
+<?php
+$string_val = "this is John Cizzel.";
+echo $string_val;
+?>
+</body>
+</html>
---
+++ Lab_03/localvar.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>
+        Local Variables
+    </title>
+</head>
+<body>
+<?php
+function sum()
+{
+    $int1 = 43.4;
+    $int2 = 13;
+    $int3 = $int1 + $int2;
+    echo "The sum of $int1 + $int2 = $int3";
+}
+sum();0
+?>
+
+</body>
+</html>
+
